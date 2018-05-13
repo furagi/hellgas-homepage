830920438275d79bd34feb15493970bb761b4f88
furagi <furagi.usagi@gmail.com>
2018-05-13 18:09:31 +1000

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as $ from 'jquery';
+import 'slick-carousel';
+import 'lightbox2';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -9,3 +12,44 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+(function () {
+  'use strict';
+
+  $(document).ready(function () {
+    // $('.hero__slick').slick({
+    //   dots: false,
+    //   infinite: true,
+    //   autoplay: true,
+    //   fade: true,
+    //   arrows: false
+    // });
+
+    $('.perfumes__slick').slick({
+      dots: false,
+      infinite: true,
+      autoplay: true,
+      // fade: true,
+      arrows: true,
+      slidesToShow: 3,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          arrows: true,
+        }
+      }, {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        }
+      }]
+    });
+
+    $('.feedback__show-more-container').click(() => {
+      $('.feedback__show-more-container').remove();
+      $('.feedback__list_folded').removeClass('feedback__list_folded');
+      $('.feedback__image.blur').removeClass('blur');
+    });
+  });
+})();
